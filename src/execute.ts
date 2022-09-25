1ba4301c6a4c40016b0fe76887c277598a5e9283
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-09-25 15:15:38 +0900

    fix(execute): set existing environment variables

--- src/execute.ts
+++ src/execute.ts
@@ -1,13 +1,12 @@
 import { spawn } from "child_process";
-import { join } from "path";
 
 export const runExecutable = (executablePath: string, secret: string) => {
   return new Promise<void>((resolve, reject) => {
-    const process = spawn(executablePath, {
+    const childProcess = spawn(executablePath, {
       stdio: "inherit",
-      env: { FT_API_SECRET: secret },
+      env: { ...process.env, FT_API_SECRET: secret },
     });
-    process.on("close", (code) => {
+    childProcess.on("close", (code) => {
       if (code) {
         reject();
       }
